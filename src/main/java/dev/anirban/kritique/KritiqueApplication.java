4b986d0817de07d09a14f0697fa02121a3ba9f4b
Anirban Basak <jeetbasak2002@gmail.com>
2024-09-23 15:19:19 +0530

    feat - SQL Server connection established.

--- src/main/java/dev/anirban/kritique/KritiqueApplication.java
+++ src/main/java/dev/anirban/kritique/KritiqueApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KritiqueApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KritiqueApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KritiqueApplication.class, args);
+    }
 }
