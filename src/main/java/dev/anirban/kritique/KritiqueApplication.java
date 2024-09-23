1262149edc72c5550a7ddf99e9de03294cc45e29
Anirban Basak <jeetbasak2002@gmail.com>
2024-09-23 15:14:17 +0530

    initial commit

---
+++ src/main/java/dev/anirban/kritique/KritiqueApplication.java
@@ -0,0 +1,13 @@
+package dev.anirban.kritique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KritiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KritiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/anirban/kritique/KritiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anirban.kritique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KritiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
