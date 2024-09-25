2c3b816d707c0904ccb7d5acd031d7789f18174f
Anirban Basak <jeetbasak2002@gmail.com>
2024-09-25 15:50:24 +0530

    Revert "feat - Readying for deployment."
    This reverts commit b99679074a344e63dd2a6bfba3cc13964a24101c.

--- src/main/java/dev/anirban/kritique/KritiqueApplication.java
+++ src/main/java/dev/anirban/kritique/KritiqueApplication.java
@@ -1,40 +1,11 @@
 package dev.anirban.kritique;
 
-import dev.anirban.kritique.dto.faculty.FacultyDTO;
-import dev.anirban.kritique.service.FacultyService;
-import dev.anirban.kritique.service.RandomNameGenerator;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class KritiqueApplication {
-
-    private final FacultyService service;
-    private final RandomNameGenerator randomNameGenerator;
-
     public static void main(String[] args) {
         SpringApplication.run(KritiqueApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return _ -> {
-            for (int i = 0; i < 50; i++) {
-                FacultyDTO facultyDTO = FacultyDTO
-                        .builder()
-                        .name(randomNameGenerator.generateRandomName())
-                        .experience(0.0)
-                        .photoUrl("")
-                        .avgRating(0.0)
-                        .totalRatings(0)
-                        .build();
-
-                service.createFaculty(facultyDTO);
-            }
-        };
     }
 }
