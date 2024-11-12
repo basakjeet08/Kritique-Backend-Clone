b0e0fd6e0e2796788d41c7a49d488db9cf256ef6
Anirban Basak <jeetbasak2002@gmail.com>
2024-11-12 16:42:29 +0530

    update - App version change

--- src/main/java/dev/anirban/kritique/constants/VersionConstant.java
+++ src/main/java/dev/anirban/kritique/constants/VersionConstant.java
@@ -2,5 +2,5 @@ package dev.anirban.kritique.constants;
 
 public class VersionConstant {
 
-    public final static String APP_VERSION = "2.0.0";
+    public final static String APP_VERSION = "2.1.0";
 }
